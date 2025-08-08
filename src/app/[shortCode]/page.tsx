82ec5510d9fcb0cb4244b93f46536f90e0592990
Arjit Chauhan <arjitchauhan76@gmail.com>
2025-08-08 21:45:47 +0530

    fix: Correct props type on server page

--- src/app/[shortCode]/page.tsx
+++ src/app/[shortCode]/page.tsx
@@ -1,13 +1,8 @@
 // src/app/[shortCode]/page.tsx
 import RedirectClient from './RedirectClient';
 
-type Props = {
-  params: {
-    shortCode: string;
-  };
-};
-
-const RedirectPage = ({ params }: Props) => {
+// We are simplifying the props definition to be directly inline.
+const RedirectPage = ({ params }: { params: { shortCode: string } }) => {
   return <RedirectClient shortCode={params.shortCode} />;
 };
 
